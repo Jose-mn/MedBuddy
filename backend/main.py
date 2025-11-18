d62cf1c78aa52ef98e530678d0c0ebf4c6b34d8d
Jose-mn <josephmuthui134@gmail.com>
2025-11-18 21:03:00 +0300

    checked urls

--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://med-buddy-delta.vercel.app",
+        "https://medbuddy-production-f52a.up.railway.app",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8000",
