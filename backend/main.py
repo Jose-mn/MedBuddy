55bf34d66ce2ca90c4eb8f79ffd6edbee1bf559d
Jose-mn <josephmuthui134@gmail.com>
2025-11-18 17:20:29 +0300

    updated docker

--- backend/main.py
+++ backend/main.py
@@ -77,4 +77,4 @@ if __name__ == "__main__":
     import os
 
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
